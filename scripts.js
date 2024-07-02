ebc3c0d131258d264cb2ff334f6997890e3b1695
daersh <daersh@naver.com>
2024-07-02 20:49:34 +0900

    test

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const posts = document.querySelectorAll('.posts li a');
-    posts.forEach(post => {
-        post.addEventListener('click', (event) => {
-            event.preventDefault();
-            alert('Post link clicked: ' + post.textContent);
-        });
-    });
-});
